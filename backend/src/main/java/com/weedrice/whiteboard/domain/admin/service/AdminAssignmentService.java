9f5f2dc1506a388241a5eb9bb20f19f3b7f8fe7f
weedrice <supp0rtyoo@gmail.com>
2026-04-22 14:40:45 +0900

    Chore: 3차 검토 반영

--- backend/src/main/java/com/weedrice/whiteboard/domain/admin/service/AdminAssignmentService.java
+++ backend/src/main/java/com/weedrice/whiteboard/domain/admin/service/AdminAssignmentService.java
@@ -15,8 +15,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class AdminAssignmentService {
