c7dbe02dadc3cb57467160a7ee35077ffa56a821
weedrice <supp0rtyoo@gmail.com>
2026-04-28 10:58:48 +0900

    Refactor: 태그 할당 조회 fetch 최적화

--- backend/src/main/java/com/weedrice/whiteboard/domain/tag/repository/PostTagRepository.java
+++ backend/src/main/java/com/weedrice/whiteboard/domain/tag/repository/PostTagRepository.java
@@ -3,11 +3,13 @@ package com.weedrice.whiteboard.domain.tag.repository;
 import com.weedrice.whiteboard.domain.post.entity.Post;
 import com.weedrice.whiteboard.domain.tag.entity.PostTag;
 import com.weedrice.whiteboard.domain.tag.entity.PostTagId;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PostTagRepository extends JpaRepository<PostTag, PostTagId> {
+    @EntityGraph(attributePaths = "tag")
     List<PostTag> findByPost(Post post);
 
     void deleteByPost(Post post);
